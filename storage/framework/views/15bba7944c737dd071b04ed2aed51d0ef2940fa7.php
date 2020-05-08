3493fab512745dcf3e2b5e2901cb3dacb46784de
rfnvokasi <mrfn.mails@gmail.com>
2020-05-09 06:20:35 +0700

    ui

---
+++ storage/framework/views/15bba7944c737dd071b04ed2aed51d0ef2940fa7.php
@@ -0,0 +1,99 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+
+    <div class="panel-header panel-header-sm">
+    </div>
+    <div class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <div class="col-lg-6">
+              <h4 class="card-title">Menu Booking</h4>
+            </div>
+              <div class="col-lg-12">
+                <button type="button" class="btn btn-success float-right" data-toggle="modal" data-target="#exampleModal">Tambah Data</button>
+            </div>
+            </div>
+           
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table" style="margin-top: 10dp;">
+                  <thead class="text-primary">
+                    <tr>
+                      <th>NAMA</th>
+                      <th>TANGGAL</th>
+                      <th>JAM</th>
+                      <th>LAYANAN</th>
+                      <th>AKSI</th>
+                  </tr>
+                  </thead>
+                  <?php $__currentLoopData = $data_booking; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $booking): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                  <tr>
+                      <td><?php echo e($booking->nama); ?></td>
+                      <td><?php echo e($booking->tgl); ?></td>
+                      <td><?php echo e($booking->jam); ?></td>
+                      <td><?php echo e($booking->layanan); ?></td>
+                      <td>
+                          <a href="/Adminbarber/<?php echo e($booking->id); ?>/delete" class="btn btn-danger" onclick="return confirm('Yakin Mau di Hapus?')">Delete</a>
+                      </td>
+                  </tr>    
+                  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                   </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <form action="/Adminbarber/create" method="POST">
+                <?php echo csrf_field(); ?>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Nama</label>
+                  <input name="nama" type="text" class="form-control" placeholder="Nama" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Tanggal</label>
+                  <input name="tgl" class="date form-control" placeholder="Tanggal" type="text">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Jam</label>
+                    <input name="jam" placeholder="Jam" class="timepicker form-control" type="text">
+                  </div>
+
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Pilih Layanan</label>
+                    <select name="layanan" class="form-control" id="exampleFormControlSelect1">
+                      <option>Cukur Biasa - Rp. 30,000</option>
+                      <option>Paket 1 - Rp. 50,000</option>
+                      <option>Paket 2 - Rp. 75,000</option>
+                    </select>
+                  </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </form>
+        </div>
+      </div>
+    </div>
+    <?php $__env->stopSection(); ?>
+
+
+   
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\AdminBarber\resources\views/Admin/index.blade.php ENDPATH**/ ?>
